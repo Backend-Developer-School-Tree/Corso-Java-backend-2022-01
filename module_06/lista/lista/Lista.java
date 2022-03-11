21aa1892c869f3b7d0c16b8f9d32b2c3693f7421
Pietro Marchetto <pietromarchetto5@gmail.com>
2022-03-11 12:06:06 +0100

    Esercizi Iterabile e Lista

---
+++ module_06/lista/lista/Lista.java
@@ -0,0 +1,15 @@
+package lista.lista;
+
+public interface Lista {
+    public Object get(int i);
+
+    public boolean contains(Object o);
+
+    public int size();
+
+    public void add(Object o);
+
+    public boolean remove(int i);
+
+    public void clear();
+}
