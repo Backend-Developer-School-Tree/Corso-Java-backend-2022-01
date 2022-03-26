6da4e7ad527a85b1b5ed74244b28208f760e8512
mel <massotti.melvin@gmail.com>
2022-03-26 13:07:15 +0100

    Esercizi modulo 12

---
+++ module_12/src/letters_count/LettersCount.java
@@ -0,0 +1,19 @@
+package letters_count;
+
+import java.util.List;
+
+public class LettersCount {
+
+    static int lettersCount(List<String> names) {
+        return names.stream()
+                .filter(name -> name.toLowerCase().startsWith("f"))
+                .map(String::length)
+                .reduce(0, Integer::sum);
+
+    }
+
+    public static void main(String[] args) {
+        List<String> names = List.of("marco", "franco", "federico", "mario");
+        System.out.println(lettersCount(names) == 14);
+    }
+}
